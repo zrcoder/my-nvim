a83150a549e0bd4b30dd0228964067e540186e21
zrcoder <love-nankai@163.com>
2023-07-03 02:14:06 +0800

    first commit

---
+++ lua/plugins/color-theme.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "folke/tokyonight.nvim",
+        opts = {
+            transparent = true,
+            style = "night",
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+        },
+    },
+    {
+        "rcarriga/nvim-notify",
+        opts = {
+            background_colour = "#000000",
+        },
+    },
+}
---
+++ lua/plugins/smart-im.lua
@@ -0,0 +1,7 @@
+return {
+    "ybian/smartim",
+    event = { "InsertEnter" },
+    config = function()
+        vim.g.smartim_default = "com.apple.keylayout.ABC"
+    end,
+}
